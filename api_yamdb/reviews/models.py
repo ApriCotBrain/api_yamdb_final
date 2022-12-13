dba93cfd2cafb47dd1bcfbc761837c1a398d51de
«vvych» <wychelm@ya.ru>
2022-12-13 10:59:25 +0700

    add apps

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Title(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
