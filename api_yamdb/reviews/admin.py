725ad542aa7c00227d813d707e5f8c04cae36b26
Ольга Мелихова <melihova.o@yandex.ru>
2022-12-14 16:06:35 +0300

    csv users title

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+from reviews.models import Category, Genre, Title
+
+
+class CategoryResource(resources.ModelResource):
+    class Meta:
+        model = Category
+        fields = (
+            'id',
+            'name',
+            'slug',
+        )
+
+
+@admin.register(Category)
+class CategoryAdmin(ImportExportModelAdmin):
+    resource_classes = [CategoryResource]
+    list_display = (
+        'name',
+        'slug',
+    )
+
+
+class GenreResource(resources.ModelResource):
+    class Meta:
+        model = Genre
+        fields = (
+            'id',
+            'name',
+            'slug',
+        )
+
+
+@admin.register(Genre)
+class GenreAdmin(ImportExportModelAdmin):
+    resource_classes = [GenreResource]
+    list_display = (
+        'name',
+        'slug',
+    )
