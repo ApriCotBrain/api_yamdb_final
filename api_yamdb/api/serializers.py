fc513d01d2fbb9816f6f18e71665445cbaa315c0
Никита Торбин <torbinnikitka@gmail.com>
2022-12-16 03:32:03 +0300

    clearmodels

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
+from django.shortcuts import get_object_or_404
+
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
+from rest_framework.relations import SlugRelatedField
 
 from reviews.models import Category, Genre, Title, Reviews, Comments
 
@@ -38,15 +41,37 @@ class GenreSerializer(serializers.ModelSerializer):
 
 
 class ReviewSerializers(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
     class Meta:
-        fields = ('id', 'title', 'text', 'score')
+        fields = '__all__'
         model = Reviews
+        read_only_fields = ('title',)
+
+    def validate(self, data):
+        title_id = self.context['view'].kwargs.get('title_id')
+        author = self.context.get('request').user
+        title = get_object_or_404(Title, id=title_id)
+        if (title.reviews.filter(author=author).exists()
+           and self.context.get('request').method != 'PATCH'):
+            raise serializers.ValidationError(
+                'Можно оставлять только один отзыв!'
+            )
+        return data
+
+    def validate_score(self, value):
+        if value < 1 or value > 10:
+            raise serializers.ValidationError('Недопустимое значение!')
+        return value
 
 
 class CommentSerializers(serializers.ModelSerializer):
-    class Meta:
-        fields = ('id', 'review', 'text')
-        model = Comments
+    author = SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
 
-# По тз 1 юзер может добавить только 1 отзыв,
-# пока не нашёл как это реализовать, если у вас есть идеи, с радостью выслушаю
+    class Meta:
+        fields = '__all__'
+        model = Comments
+        read_only_fields = ('review',)
+
