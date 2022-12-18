73428a9200a69c4dc5a081123bec9634e6998eb4
«vvych» <wychelm@ya.ru>
2022-12-19 02:37:03 +0700

    end-day update

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,23 +3,38 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_admin
+        return (
+            request.user.is_admin
+            or request.user.is_staff
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.user.is_admin
+            or request.user.is_staff
+        )
 
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return (
-                request.method in SAFE_METHODS
-                or (request.user.is_authenticated and request.user.is_admin)
-        )
+        if request.method in SAFE_METHODS:
+            return True
+        if request.user.is_authenticated:
+            return request.user.is_admin
+        return False
 
 
 class HasRoleOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
     def has_object_permission(self, request, view, obj):
         return (
             request.method in SAFE_METHODS
-            or request.method == 'POST' and request.user.is_authenticated
-            or request.user.is_admin
-            or request.user.is_moderator
             or obj.author == request.user
+            or request.user.is_moderator
+            or request.user.is_admin
         )
