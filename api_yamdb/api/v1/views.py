e0078d81e502a342e9da375a9d42e151f4da9391
Никита Торбин <torbinnikitka@gmail.com>
2022-12-21 12:27:12 +0300

    comment

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -168,7 +168,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = [HasRoleOrReadOnly, IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
-        review = get_object_or_404(Review, id=self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'),
                                     title__id=self.kwargs.get('title_id'))
         return review.comments.all()
 
